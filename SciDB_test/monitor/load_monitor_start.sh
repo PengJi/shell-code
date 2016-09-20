becaee5653a21d7f17c30d2afeb8b987c5fcf08a
pengji <jipeng92@gmail.com>
2016-09-20 09:44:49 +0800

    modified:   load_monitor_start.sh

--- SciDB_test/monitor/load_monitor_start.sh
+++ SciDB_test/monitor/load_monitor_start.sh
@@ -1,13 +1,13 @@
 :<<doc
 创建monitor*文件
-与monitor_start.sh文件不同之处在于，这里是以2S为时间间隔
+与monitor_start.sh文件不同之处在于，这里是以3S为时间间隔
 doc
 
 echo -e "\033[33;49;1m [JPDB2 start] \033[39;49;0m"
 
 ssh gpadmin@JPDB2 << eof
 echo "******************************************************************************************************************************" >> /tmp/monitor.txt
-collectl -scmdn -oT -i2 >> /tmp/monitor.txt &
+collectl -scmdn -oT -i3 >> /tmp/monitor.txt &
 eof
 
 for k in $(seq 1 6)
@@ -15,7 +15,7 @@ do
 echo -e "\033[33;49;1m [node${k} start] \033[39;49;0m"
 ssh gpadmin@node${k} << eof
 echo "******************************************************************************************************************************" >> /tmp/monitor1.txt
-collectl -scmdn -oT -i2 >> /tmp/monitor${k}.txt &
+collectl -scmdn -oT -i3 >> /tmp/monitor${k}.txt &
 eof
 done
 
