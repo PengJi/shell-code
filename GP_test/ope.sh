6b46f9e015cda6071ecf582bb5fd2d2c333d76fd
pengji <jipeng92@gmail.com>
2016-10-13 11:11:18 +0800

    modified:   GP_test/ope.sh

--- GP_test/ope.sh
+++ GP_test/ope.sh
@@ -15,7 +15,7 @@ doc
 # 准备工作
 # 参数：
 # 操作类型：p
-if [ "$1" = "p"]; then
+if [ "$1" = "p" ]; then
 	createDirFun
 	delLoadResFun
 	delQueryResFun
@@ -24,14 +24,14 @@ fi
 # 清空表
 # 参数：
 # 操作类型：q
-if [ "$1" = ""]; then
+if [ "$1" = "q" ]; then
 	truncateTableFun
 fi
 
 # 删除表
 # 参数：
 # 操作类型：d
-if [ "$1" = "d"]; then
+if [ "$1" = "d" ]; then
 	dropTableFun
 fi
 
@@ -42,7 +42,7 @@ fi
 # 表名：galaxylj/photoobjall/photoprimarylj/StarLJ/neighbors
 # 参数3：
 # 表的类型：a/ac/ao/aoc/空(默认)
-if [ "$1" = "t"]; then
+if [ "$1" = "t" ]; then
 	createTableFun $2 $3
 fi
 
@@ -51,29 +51,66 @@ fi
 # 操作类型：l
 # 参数2
 # 数据大小：10、20、50、100
-if [ "$1" = "l"]; then
+if [ "$1" = "l" ]; then
 	loadTable $2
+fi
+
+# 参数1：
+# 操作类型：l
+# 参数2
+# 表名：galaxylj/photoobjall/photoprimarylj/starlj/neighbors
+# 参数3
+# 数据大小：10、20、50、100
+# 导入某个表
+if [ "$1" = "ll" ]; then
+	if [ "$2" = "galaxylj" ]; then
+        if [ ! -d "./rec_load" ]; then
+            mkdir ./rec_load
+        fi  
+        loadGalaxyljFun $3
+    elif [ "$2" = "photoobjall" ]; then
+        if [ ! -d "./rec_load" ]; then
+            mkdir ./rec_load
+        fi  
+        loadPhotoobjallFun $3
+    elif [ "$2" = "photoprimarylj" ]; then
+        if [ ! -d "./rec_load" ]; then
+            mkdir ./rec_load
+        fi  
+        loadPhotoprimaryljFun $3
+    elif [ "$2" = "starlj" ]; then
+        if [ ! -d "./rec_load" ]; then
+            mkdir ./rec_load
+        fi  
+        loadStarljFun $3
+    elif [ "$2" = "neighbors" ]; then
+        if [ ! -d "./rec_load" ]; then
+            mkdir ./rec_load
+        fi  
+        loadneighborsFun $3
+    else
+        echo -e "\033[31;49;1m [table not exists] \033[39;49;0m"
+    fi 
 fi
 
 # 查询表
 # 参数
 # 操作类型：s
-if [ "$1" = "s"]; then
+if [ "$1" = "s" ]; then
 	queryTableFun
 fi
 
 # 导入和查询数据
 # 参数
 # 操作类型：ls
-if [ "$1" = "ls"]; then
+if [ "$1" = "ls" ]; then
 	
 fi
 
 # 结尾工作
 # 参数：
 # 操作类型：e
-if [ "$1" = "e"]; then
-	
+if [ "$1" = "e" ]; then
     # 删除表
     delTable
     # 清空缓存
