0555242423aa840a0aba3795a411f5327a4664cb
pengji <jipeng92@gmail.com>
2016-10-21 23:15:10 +0800

    modified:   SciDB_test/sql/photoprimarylj-2_1.sql 	modified:   SciDB_test/sql/photoprimarylj-2_2.sql

--- SciDB_test/sql/photoprimarylj-2_1.sql
+++ SciDB_test/sql/photoprimarylj-2_1.sql
@@ -5,6 +5,5 @@ set lang aql;
 SELECT 
 	P.objID 
 FROM 
-	PhotoPrimaryLJ AS P join neighbors AS N on P.objID =N.NeighborObjID
-WHERE 
-	P.objID = N.objID;
+	PhotoPrimaryLJ AS P join neighbors AS N on P.objID =N.NeighborObjID and P.objID = N.objID;
+
--- SciDB_test/sql/photoprimarylj-2_2.sql
+++ SciDB_test/sql/photoprimarylj-2_2.sql
@@ -5,6 +5,5 @@ set lang aql;
 SELECT 
 	P.objID 
 FROM 
-	neighbors AS N join PhotoPrimaryLJ AS P on P.objID =N.NeighborObjID
-WHERE 
-	P.objID = N.objID;
+	neighbors AS N join PhotoPrimaryLJ AS P on P.objID =N.NeighborObjID and P.objID = N.objID; 
+
